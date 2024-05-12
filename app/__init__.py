4cfc2c11f5b98151bd9167c6224ecd4b8020960e
JHLgithuber <jfttstudio@gmail.com>
2024-05-12 14:49:35 +0900

    maked sample flask-socketio project

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,67 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+#from app import app
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-from app import routes
+logs = []  # 로그를 저장할 리스트
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/logs')
+def show_logs():
+    return jsonify(logs)
+
+
+@socketio.on('send_message')
+def handle_message(message):
+    print(f"Received message: {message}")
+
+    user_id = message.get('user').get('_id')
+    log_entry = {
+        'from': user_id,
+        'JSON_DATA': message
+    }
+
+    logs.append(log_entry)  # 로그 저장
+    """
+    print(f"Log: {log_entry}")
+    """
+
+    socketio.emit('receive_message', message)
+    print(f"Sendied message: {message}")
+
+
+@socketio.on('request_public_key')
+def handle_request_public_key(data):
+    print(f"Received request_public_key: {data}")
+    """
+    user_id = data.get('user').get('_id')
+    log_entry = {
+        'from': user_id,
+        'JSON_DATA': data
+    }
+
+    logs.append(log_entry)  # 로그 저장
+    """
+    socketio.emit('receive_request_public_key', data)
+    print(f"Sendied request_public_key: {data}")
+
+
+@socketio.on('response_public_key')
+def handle_public_key(public_key):
+    print(f"Received response_public_key: {public_key}")
+    """
+    user_id = public_key.get('user').get('_id')
+    log_entry = {
+        'from': user_id,
+        'JSON_DATA': public_key
+    }
+
+    logs.append(log_entry)  # 로그 저장
+    """
+    socketio.emit('receive_response_public_key', public_key)
+    print(f"Sendied response_public_key: {public_key}")
