9fd6de5d41963a7a53aeb5fb1785ae2e3f8d6cbf
JHLgithuber <jfttstudio@gmail.com>
2024-05-12 17:45:23 +0900

    SSL_Encription

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from app import app
+import ssl
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(certfile='cert.pem', keyfile='key.pem', password=os.getenv('SSL_PW'))
+    app.run(ssl_context=ssl_context, host='0.0.0.0', port=5000, debug=True)
