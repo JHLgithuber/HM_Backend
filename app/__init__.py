f8b43742f2f5c0a177dcd4922ebe46304a01b156
JHLgithuber <jfttstudio@gmail.com>
2024-05-12 15:17:54 +0900

    making logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify
-#from app import app
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -9,6 +8,7 @@ logs = []  # 로그를 저장할 리스트
 
 @app.route('/')
 def index():
+    app.logger.info('인덱스 페이지에 접근됨')
     return render_template('index.html')
 
 @app.route('/logs')
@@ -18,7 +18,7 @@ def show_logs():
 
 @socketio.on('send_message')
 def handle_message(message):
-    print(f"Received message: {message}")
+    app.logger.info(f"Received message: {message}")
 
     user_id = message.get('user').get('_id')
     log_entry = {
@@ -28,16 +28,16 @@ def handle_message(message):
 
     logs.append(log_entry)  # 로그 저장
     """
-    print(f"Log: {log_entry}")
+    logging(f"Log: {log_entry}")
     """
 
     socketio.emit('receive_message', message)
-    print(f"Sendied message: {message}")
+    app.logger.info(f"Sendied message: {message}")
 
 
 @socketio.on('request_public_key')
 def handle_request_public_key(data):
-    print(f"Received request_public_key: {data}")
+    app.logger.info(f"Received request_public_key: {data}")
     """
     user_id = data.get('user').get('_id')
     log_entry = {
@@ -48,12 +48,12 @@ def handle_request_public_key(data):
     logs.append(log_entry)  # 로그 저장
     """
     socketio.emit('receive_request_public_key', data)
-    print(f"Sendied request_public_key: {data}")
+    app.logger.info(f"Sendied request_public_key: {data}")
 
 
 @socketio.on('response_public_key')
 def handle_public_key(public_key):
-    print(f"Received response_public_key: {public_key}")
+    app.logger.info(f"Received response_public_key: {public_key}")
     """
     user_id = public_key.get('user').get('_id')
     log_entry = {
@@ -64,4 +64,4 @@ def handle_public_key(public_key):
     logs.append(log_entry)  # 로그 저장
     """
     socketio.emit('receive_response_public_key', public_key)
-    print(f"Sendied response_public_key: {public_key}")
+    app.logger.info(f"Sendied response_public_key: {public_key}")
