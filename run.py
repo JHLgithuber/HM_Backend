ca5c9dda1cae792b2215e9e18429118c5b6f106a
JHLgithuber <jfttstudio@gmail.com>
2024-05-12 19:49:22 +0900

    delete Key

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 if __name__ == "__main__":
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    ssl_context.load_cert_chain(certfile='cert.pem', keyfile='key.pem', password=os.getenv('SSL_PW'))
+    ssl_context.load_cert_chain(certfile=os.getenv('SSL_CERT'), keyfile=os.getenv('SSL_KEY'), password=os.getenv('SSL_PW'))
     app.run(ssl_context=ssl_context, host='0.0.0.0', port=5000, debug=True)
